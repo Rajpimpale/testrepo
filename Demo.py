28ed130050f9b1e899c324d42aaad0246ea3d31d
RajPimpale <76784614+Rajpimpale@users.noreply.github.com>
2020-12-30 22:02:44 +0530

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 15:49:45 2020
+
+@author: Gori
+"""
+
+print("INDIA")
+
+x = int(input ("entrer number: "))
+
+if x % 2 == 0:
+ print("enter number is even")
+    
+else :
+ print(" enter no is odd ")
+
